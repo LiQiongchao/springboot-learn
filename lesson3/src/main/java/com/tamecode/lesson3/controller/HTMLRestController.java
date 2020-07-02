2f7bdbce9aea65a438f433eb65c4468aa5c0748f
liqiongchao-home <liqiongchao2016@gmail.com>
2020-07-02 22:08:20 +0800

    html的注解的使用

--- lesson3/src/main/java/com/tamecode/lesson3/controller/HTMLRestController.java
+++ lesson3/src/main/java/com/tamecode/lesson3/controller/HTMLRestController.java
@@ -1,9 +1,15 @@
 package com.tamecode.lesson3.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -32,6 +38,40 @@ public class HTMLRestController {
     @GetMapping(path = "/html/demo/{msg}")
     public String htmlPathVariable(@PathVariable String msg) {
         return "<html><body><h1>Hello, "+msg+"</h1></body></html>";
+    }
+
+    // @RequestParam
+    @GetMapping(path = "/html/demo/params")
+    public String htmlParams(@RequestParam(value = "p", required = false, defaultValue = "empty") String name
+            , @RequestParam(required = false, defaultValue = "0") Integer age
+            , HttpServletRequest request) {
+        String favour = request.getParameter("favour");
+        return "<html><body><h1>Hello, "+name+"</h1> you age is "+ age + " ,favour is "+ favour +" </body></html>";
+    }
+
+    // @RequestHeader
+    @GetMapping(path = "/html/demo/header")
+    public String htmlHeader(@RequestHeader(value = "Accept") String acceptType
+            , HttpServletRequest request) {
+//        String accept = request.getHeader("Accept");
+        return "<html><body><h1>Accept = "+acceptType+"</h1></body></html>";
+    }
+
+    // @CookieValue
+    @GetMapping(path = "/html/demo/cookie")
+    public String htmlCookie(@CookieValue(required = false, defaultValue = "empty") String app_key
+            , HttpServletRequest request) {
+        return "<html><body><h1>cookie app_key = "+app_key+"</h1></body></html>";
+    }
+
+    // ResponseEntity 可以定制响应头信息
+    @GetMapping(path = "/html/demo/resp")
+    public ResponseEntity htmlEntity(HttpServletRequest request) {
+        String html = "<html><body><h1>response entity</h1></body></html>";
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("MyHeader", Arrays.asList("myValue"));
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(html, headers, HttpStatus.OK);
+        return responseEntity;
     }
 
 
