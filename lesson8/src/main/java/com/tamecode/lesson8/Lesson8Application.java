f1f743f66886690cf657ff1afe5ad9369e6335d7
liqiongchao-home <liqiongchao2016@gmail.com>
2020-08-22 19:24:23 +0800

    开启beans的查看

--- lesson8/src/main/java/com/tamecode/lesson8/Lesson8Application.java
+++ lesson8/src/main/java/com/tamecode/lesson8/Lesson8Application.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+// 使用JPARepository
 @EnableJpaRepositories
+// 因为repository没有使用接口，所以要使用CGlib代理，无法使用JDK动态代理
 @EnableTransactionManagement(proxyTargetClass = true)
 public class Lesson8Application {
 
