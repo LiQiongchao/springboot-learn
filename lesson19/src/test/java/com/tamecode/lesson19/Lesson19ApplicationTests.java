496919d40fd6237dfa5e891f5666460800d8a442
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-09-24 14:39:02 +0800

    单元测试项目搭建

---
+++ lesson19/src/test/java/com/tamecode/lesson19/Lesson19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamecode.lesson19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
