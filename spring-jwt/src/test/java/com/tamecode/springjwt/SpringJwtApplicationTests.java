f0a9db08f3ff2d7c85d0539924e6d00ab2fa5695
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-10-16 14:39:43 +0800

    jwt工具类

---
+++ spring-jwt/src/test/java/com/tamecode/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamecode.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
