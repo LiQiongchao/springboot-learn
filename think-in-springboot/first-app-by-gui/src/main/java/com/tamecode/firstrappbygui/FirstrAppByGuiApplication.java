4aebc680679d4561ccfd748a71f33b1556e52e1a
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-12-03 14:08:28 +0800

    first by gui springboot

---
+++ think-in-springboot/first-app-by-gui/src/main/java/com/tamecode/firstrappbygui/FirstrAppByGuiApplication.java
@@ -0,0 +1,13 @@
+package com.tamecode.firstrappbygui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstrAppByGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstrAppByGuiApplication.class, args);
+    }
+
+}
---
+++ think-in-springboot/first-app-by-gui/src/test/java/com/tamecode/firstrappbygui/FirstrAppByGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamecode.firstrappbygui;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrAppByGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
