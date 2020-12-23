95784cf308ab376f7c48f33e312890bb8651e352
liqiongchao-home <liqiongchao2016@gmail.com>
2020-12-23 23:08:32 +0800

    增加说明

--- think-in-springboot/first-app-by-gui/src/main/java/com/tamecode/autoconfig/WebAutoConfiguration.java
+++ think-in-springboot/first-app-by-gui/src/main/java/com/tamecode/autoconfig/WebAutoConfiguration.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Import;
  * @Date: 2020/12/23 21:57
  */
 @Configuration
-// 使用 @Import 导入 {@link WebConfiguration}
+// 使用 @Import 导入 {@link WebConfiguration} 纳入spring 容器中，并且是全类名，
+// 如: com.tamecode.config.WebConfiguration。(@Bean导入的是 webConfiguration)
 @Import(WebConfiguration.class)
 public class WebAutoConfiguration {
 }
